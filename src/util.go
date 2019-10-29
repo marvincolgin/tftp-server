47678c303fe0436d64280166cea26e400c3b2617
Marvin Colgin <bpadmin@builtbp.com>
2019-10-29 11:51:20 -0700

    ADD: util will contain helper func

---
+++ src/util.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+// fileExists determines if the fileexists and it's *NOT* a directory
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
